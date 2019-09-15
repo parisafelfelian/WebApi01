66e0b21f76d681557c7f06c0d364c04baf6c9c32
parinaz.felfelian <parinaz.felfelian@gmail.com>
2019-09-15 12:00:25 +0430

    Add project files.

---
+++ WebApi_01/Models/TodoRepository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Concurrent;
+
+namespace WebApi_01.Models
+{
+    public class TodoRepository : ITodoRepository
+    {
+        private static ConcurrentDictionary<string, TodoItem> _Todos = new ConcurrentDictionary<string, TodoItem>();
+
+        public TodoRepository()
+        {
+            Add(new TodoItem { Name = "Item01" });
+        }
+        public void Add(TodoItem item)
+        {
+            item.Key = Guid.NewGuid().ToString();
+            _Todos[item.Key] = item;
+        }
+
+        public TodoItem Find(string key)
+        {
+            TodoItem item;
+            _Todos.TryGetValue(key, out item);
+            return item;
+        }
+        public void Update(TodoItem item)
+        {
+            _Todos[item.Key] = item;
+        }
+
+        public TodoItem Remove(string key)
+        {
+            TodoItem item;
+            _Todos.TryRemove(key, out item);
+            return item;
+        }
+
+
+        public IEnumerable<TodoItem> GetAll()
+        {
+            return _Todos.Values;
+        }
+
+    }
+}
